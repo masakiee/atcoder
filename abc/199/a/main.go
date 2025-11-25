f2e5aa6ef76185fc0f089f3bb0ee666af1be4176
Masaki Inoue <masaki.yagui@gmail.com>
2025-11-26 08:23:48 +0900

    ABC199 - A問題

---
+++ abc/199/a/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A, B, C int
+	fmt.Scanf("%d %d %d", &A, &B, &C)
+	
+	if A*A + B*B < C*C {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
